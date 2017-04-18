79ade28a76b8be2d4d4ae55d05988909c6e6fdb0
GenuinePro <magician_blck@yahoo.com>
2017-04-18 17:40:03 -0400

    Added event and map fragments and changes made to event and map xml

--- app/src/main/java/com/example/bjaso/cs3714finalproj/MainActivity.java
+++ app/src/main/java/com/example/bjaso/cs3714finalproj/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.bjaso.cs3714finalproj;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
+
 
 /**
  * Created by bjaso on 4/4/2017.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
-    private Fragment TrailFragment;
+    private android.support.v4.app.Fragment TrailFragment, eventFragment, mapFragment;
+    private ImageView image;
+    private Button map;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event);
+
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
