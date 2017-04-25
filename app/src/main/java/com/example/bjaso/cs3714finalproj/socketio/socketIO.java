65ab85aaebf4db4918431f345771ae6fcb3a82c8
Junho Lee <kilg.dl@gmail.com>
2017-04-25 17:34:01 -0400

    login works, service skeleton created, setting being worked on

---
+++ app/src/main/java/com/example/bjaso/cs3714finalproj/socketio/socketIO.java
@@ -0,0 +1,28 @@
+package com.example.bjaso.cs3714finalproj.socketio;
+
+import android.app.Application;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+/**
+ * Created by root on 4/25/17.
+ */
+
+public class socketIO extends Application {
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://128.173.239.242/");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //this method is called from various Android components that want to use SocketIO
+    public Socket getSocket() {
+        return mSocket;
+    }
+}
